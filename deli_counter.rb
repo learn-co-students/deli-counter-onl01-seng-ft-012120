c2871a8eccddb5e5b51aea3772879cb076f1642b
Khalid T <khalid.tissire@gmail.com>
2020-01-24 06:58:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,27 @@
-def takeANumber(katzDeliLine,newPerson)
+def take_a_number(katzDeliLine,newPerson)
   katzDeliLine.push(newPerson)
   len = katzDeliLine.length
-  return "Welcome, #{katzDeliLine[len-1]}. You are number #{len} in line."
+  puts "Welcome, #{katzDeliLine.last}. You are number #{len} in line."
 end
 
-def nowServing(katzDeliLine)
+def now_serving(katzDeliLine)
   len = katzDeliLine.length
   if (len > 0) 
-    let firstPersonInLine = katzDeliLine[0]
+    firstPersonInLine = katzDeliLine[0]
     katzDeliLine.shift()
-    return "Currently serving #{firstPersonInLine}."
+    puts "Currently serving #{firstPersonInLine}."
   else
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   end
 end
 
-def currentLine(katzDeliLine)
-  len = katzDeliLine.length-1
+def line(katzDeliLine)
+  len = katzDeliLine.length
   if (len > 0)
     str = "The line is currently: "
-    for i in 0..len do
-  #for (let i=0; i<len-1; i++)
-    str += "#{i+1}. #{katzDeliLine[i]}"
-  end
-  return str + "#{len}. #{katzDeliLine[len-1]}"
+    katzDeliLine.each_with_index { |n,i| str += (i === len-1) ? "#{i+1}. #{n}" : "#{i+1}. #{n} "}
+    puts str
   else
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
 end
