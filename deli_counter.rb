ab75d125a545262a1de4853fec7c603b02668bb7
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 18:42:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line (deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |name, index| 
-      current_line << " #{index}. #{name}"
+    deli.each_with_index do |name, index| 
+      current_line << " #{index + 1}. #{name}"
     end
     puts current_line
   end
